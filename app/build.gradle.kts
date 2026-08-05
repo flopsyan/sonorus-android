cc70076a7da66dc426bd6d902601fa3af00184b6
flopsyan <98626108+flopsyan@users.noreply.github.com>
2026-08-05 12:07:30 +0200

    Bump to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "eu.flopsyan.sonorus"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     signingConfigs {
